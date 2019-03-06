58a07849b457ac915ec2e62cebc4475411d18b7f
Óscar <36045526+oscarblanq@users.noreply.github.com>
2019-03-06 16:42:10 +0100

    Implementado algoritmo recursivo

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,6 +8,7 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
+        int mR = 0;
         public void mover_disco(Pila a, Pila b)
         {
             if (a.Top < b.Top) b.push(a.pop());
@@ -17,13 +18,12 @@ namespace Torres_de_Hanoi
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
             int m = 0;
-
             //Creación e introducción de los discos en la pila inicial
             for (int i = n; i > 0; i--) ini.push(new Disco(i));
-            //Mostrar por pantalla los elementos del disco
+            //Imprimir los elementos del disco
             for (int i = 0; i <= ini.Elementos.Count - 1; i++)
             {
-                Console.WriteLine("Valores de los discos en la pila[" + i + "]: " + ini.Elementos[i].Valor);
+                Console.WriteLine("Valores de los discos en la pila ini[" + i + "]: " + ini.Elementos[i].Valor);
             }
             Console.WriteLine();
             Console.WriteLine("Número de discos en las pilas => INI: " + ini.Size + " AUX: " + aux.Size + " FIN: " + fin.Size);
@@ -85,9 +85,39 @@ namespace Torres_de_Hanoi
                     if (fin.Size == n) break;
                 }
             }
-
             Console.WriteLine("________________________________________________________________________________");
             return m;
+        }
+        
+        public int recursivo(int n, Pila ini, Pila fin, Pila aux)
+        {
+            
+            if (n == 1)
+            {
+                mover_disco(ini, fin); mR++;
+            }
+            else
+            {
+                recursivo(n - 1, ini, aux, fin); 
+                Console.WriteLine("________________________________________________________________________________");
+                Console.WriteLine("MOVIMIENTO Nº" + mR + " REALIZADO: ");
+                Console.WriteLine("Número de discos en las pilas => INI: " + ini.Size + " AUX: " + aux.Size + " FIN: " + fin.Size);
+                Console.WriteLine("TOP en las pilas => INI: " + ini.Top + " AUX: " + aux.Top + " FIN: " + fin.Top);
+
+                mover_disco(ini, fin); mR++;
+
+                Console.WriteLine("________________________________________________________________________________");
+                Console.WriteLine("MOVIMIENTO Nº" + mR + " REALIZADO: ");
+                Console.WriteLine("Número de discos en las pilas => INI: " + ini.Size + " AUX: " + aux.Size + " FIN: " + fin.Size);
+                Console.WriteLine("TOP en las pilas => INI: " + ini.Top + " AUX: " + aux.Top + " FIN: " + fin.Top);
+
+                recursivo(n - 1, aux, fin, ini);
+                Console.WriteLine("________________________________________________________________________________");
+                Console.WriteLine("MOVIMIENTO Nº" + mR + " REALIZADO: ");
+                Console.WriteLine("Número de discos en las pilas => INI: " + ini.Size + " AUX: " + aux.Size + " FIN: " + fin.Size);
+                Console.WriteLine("TOP en las pilas => INI: " + ini.Top + " AUX: " + aux.Top + " FIN: " + fin.Top);
+            }
+            return mR;
         }
 
     }
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -16,13 +16,50 @@ namespace Torres_de_Hanoi
 
             Hanoi juego = new Hanoi();
 
+            //Número de discos con los que se va a jugar
             int numero = 3;
+            
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("             ALGORITMO ITERATIVO");
+            Console.WriteLine("********************************************************************************");
 
             int movimientos = juego.iterativo(numero, ini, fin, aux);
 
-            Console.WriteLine("El número de movimientos es: "+movimientos+".");
+            Console.WriteLine("El número de movimientos es: " + movimientos + ".");
 
             if (movimientos == Math.Pow(2, numero) - 1) Console.WriteLine("El juego es correcto.");
+
+            Console.WriteLine();
+
+            //Una vez terminado el algoritmo iterativo, borro los discos introducidos en
+            //la pila final para volver a reutilizarla antes de empezar el siguiente algoritmo
+            for (int i = numero; i > 0; i--) fin.pop();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("             ALGORITMO RECURSIVO");
+            Console.WriteLine("********************************************************************************");
+
+            //Creación e introducción de los discos en la pila inicial
+            for (int i = numero; i > 0; i--) ini.push(new Disco(i));
+
+            //Imprimir los elementos del disco
+            for (int i = 0; i <= ini.Elementos.Count - 1; i++)
+            {
+                Console.WriteLine("Valores de los discos en la pila ini[" + i + "]: " + ini.Elementos[i].Valor);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Número de discos en las pilas => INI: " + ini.Size + " AUX: " + aux.Size + " FIN: " + fin.Size);
+            Console.WriteLine("TOP en las pilas => INI: " + ini.Top + " AUX: " + aux.Top + " FIN: " + fin.Top);
+            Console.WriteLine();
+            Console.WriteLine("EMPIEZA EL JUEGO");
+
+            int movimientosR = juego.recursivo(numero, ini, fin, aux);
+
+            Console.WriteLine("________________________________________________________________________________");
+            Console.WriteLine("El número de movimientos es: "+movimientosR+".");
+
+            if (movimientosR == Math.Pow(2, numero) - 1) Console.WriteLine("El juego es correcto.");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
